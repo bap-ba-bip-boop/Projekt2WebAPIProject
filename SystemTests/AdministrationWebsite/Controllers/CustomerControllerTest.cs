ea7522dcd11839593fe7be55b0a062488b230cf1
Calle <calle.be@telia.com>
2022-05-24 21:24:30 +0200

    g

--- SystemTests/AdministrationWebsite/Controllers/CustomerControllerTest.cs
+++ SystemTests/AdministrationWebsite/Controllers/CustomerControllerTest.cs
@@ -1,18 +1,32 @@
+using AdministartionWebsite.Controllers;
+using AdministartionWebsite.Infrastructure.Profiles;
+using AutoMapper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedResources.Data;
+using SystemTests.Services;
 
 namespace SystemTests.AdministrationWebsite.Controllers;
 
 [TestClass]
 public class CustomerControllerTest
 {
+    private readonly ApplicationDbContext _context;
     private readonly CustomerController _sut;
+    private readonly IMapper _mapper;
     public CustomerControllerTest()
     {
-        
+        _mapper = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<CustomerProfile>();
+        }
+        ).CreateMapper();
+        _context = TestDatabaseService.CreateTestContext(nameof(CustomerControllerTest));
+        _sut = new CustomerController(_context,_mapper);
     }
     [TestMethod]
     public void When_Call_CustomerIndex_Should_Return()
     {
-
+        var result = _sut.CustomerIndex();
+        Assert.IsTrue(false);
     }
 }
