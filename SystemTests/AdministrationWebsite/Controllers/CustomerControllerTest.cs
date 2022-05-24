282c8f17efb5f835327223fd80d9f47d641aaa11
Calle <calle.be@telia.com>
2022-05-24 21:05:09 +0200

    e

---
+++ SystemTests/AdministrationWebsite/Controllers/CustomerControllerTest.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SystemTests.AdministrationWebsite.Controllers;
+
+[TestClass]
+public class CustomerControllerTest
+{
+    private readonly CustomerController _sut;
+    public CustomerControllerTest()
+    {
+        
+    }
+    [TestMethod]
+    public void When_Call_CustomerIndex_Should_Return()
+    {
+
+    }
+}
